ca143302451ea20c4b0670d1a77f7474532ff256
Xuan Zhang <xuaninbox@gmail.com>
2020-01-03 21:15:01 -0800

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const aws = require('aws-sdk');
+const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
+
+async function run() {
+    try {
+        const taskDefinitionPath = core.getInput('task-definition-path', {
+            required: true
+        });
+
+        const taskDefinition = core.getInput('task-definition', {
+            required: true
+        });
+
+        const ecs = new aws.ECS({
+            customUserAgent: 'amazon-ecs-download-task-definition-for-github-actions'
+        });
+        ecs.describeTaskDefinition({
+            taskDefinition: taskDefinition
+        }, function (err, data) {
+            if (err) {
+                throw new Error(`Fail to describe task definition: ${err.message}`);
+            } else {
+                const taskDefPath = path.isAbsolute(taskDefinitionPath) ?
+                    taskDefinitionPath :
+                    path.join(process.env.GITHUB_WORKSPACE, taskDefinitionPath);
+                fs.writeFile(taskDefPath, JSON.stringify(data), 'utf8', function (err) {
+                    if (err) {
+                        throw new Error(`Fail to write task definition: ${err.message}`);
+                    }
+                    core.debug(`Downloaded task definition: ${taskDefPath}`);
+                });
+            }
+        });
+    } catch (error) {
+        core.debug(error);
+        core.debug(error.stack);
+        core.setFailed(error.message);
+    }
+}
+
+module.exports = run;
+
+if (require.main === module) {
+    run();
+}
